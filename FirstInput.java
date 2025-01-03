b7634f7faa4cbcd99a145e0dee734ee96f28ad39
Harsh Kumar gupta <hkg8558@gmail.com>
2025-01-03 22:47:07 +0530

    files added

---
+++ FirstInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class FirstInput{
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    // String name = sc.next(); //take first world
+    String name = sc.nextLine();//take whole line
+    int age = sc.nextInt();//take integer,doble ,float
+    System.out.println(name);
+    System.out.println(age);
+  }
+}
